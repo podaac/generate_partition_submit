01efabe49095e4f5103f69de3c09c870c0fc511a
Nikki Tebaldi <17799906+nikki-t@users.noreply.github.com>
2023-01-25 15:20:41 -0500

    Test Submit job organization ops

--- tests/test_PartitionSubmit.py
+++ tests/test_PartitionSubmit.py
@@ -9,29 +9,21 @@ import numpy as np
 
 # Local imports
 from partition_submit.partition import Partition
-
-# test_dir = pathlib.Path(__file__).parent / "test_data"
-# dlc_lists = ["modis_aqua_filelist.txt.daily_2023_024_date_2023_01_24", "modis_aqua_filelist.txt.daily_2023_025_date_2023_01_25", "modis_aqua_filelist.txt.daily_2022_156_date_2022_06_05"]
-# partition = Partition("aqua", dlc_lists, test_dir.parent, "podaac-sndbx-generate")
-# partition.load_downloads(None, None, None, "podaac-sndbx-generate")
-# print(partition.unmatched)
-# print("---")
-# # print(partition.sst_dict)
-# for ptype, sst_files in partition.sst_dict.items():
-#     print(ptype)
-#     for k,v in sst_files.items():
-#         print("\t", k)
-#         print("\t\t", v)
-
+from partition_submit.submit import Submit
 
 class TestPartitionSubmit(unittest.TestCase):
     """Tests partition and job organization methods of Partition and Submit."""
     
     TEST_DIR = pathlib.Path(__file__).parent / "test_data"
+    
     SST_DICT = TEST_DIR.joinpath("partition_sst_dict.json")
     UNMATCHED = TEST_DIR.joinpath("partition_unmatched.json")
     PARTITION_RESULTS = TEST_DIR.joinpath("partition_results.json")
     UNMATCHED_RESULTS = TEST_DIR.joinpath("partition_unmatched_results.json")
+    
+    SUBMIT_PARTITIONS = TEST_DIR.joinpath("submit_partitions.json")
+    SUBMIT_CONFIG = TEST_DIR.joinpath("job_config.json")
+    SUBMIT_RESULTS = TEST_DIR.joinpath("submit_results.json")
     
     @patch("partition_submit.partition.get_num_lic_avil", autospec=True)
     def test_partition_chunk_downloads_job_array(self, num_lic_mock):
@@ -73,3 +65,26 @@ class TestPartitionSubmit(unittest.TestCase):
         
         for i in range(len(partition.unmatched)):
             np.testing.assert_array_equal(partition.unmatched[i].tolist(), unmatched_results[i])
+            
+    def test_submit_create_jobs(self):
+        """Test create_jobs method of Submit class."""
+        
+        # Initialize Submit class
+        submit = Submit(self.SUBMIT_CONFIG, "aqua", self.TEST_DIR.joinpath("out"))
+        
+        # Load test data
+        with open(self.SUBMIT_PARTITIONS) as jf:
+            partitions = json.load(jf)
+        
+        # Execute method
+        job_list = submit.create_jobs(partitions, "podaac-sndbx-generate")
+        
+        # Assert expected results
+        actual = []
+        for jobs in job_list:
+            actual.append([job.job_name for job in jobs])
+        with open(self.SUBMIT_RESULTS) as jf:
+            expected = json.load(jf)
+        actual = np.array(actual, dtype="object")
+        expected = np.array(expected, dtype="object")
+        np.testing.assert_array_equal(actual, expected)
