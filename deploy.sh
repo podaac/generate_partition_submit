c019fa4863572a0561e224b8c1ad5671d3613009
Nikki Tebaldi <17799906+nikki-t@users.noreply.github.com>
2023-04-25 16:19:44 +0000

    Wait for function to be deployed

--- deploy.sh
+++ deploy.sh
@@ -6,12 +6,15 @@
 # [1] function_name: Name of AWS Lambda function name
 # [2] image_uri: URI of container of image
 # 
-# Example usage: ./delpy.sh "my-lambda-function" "account-id.dkr.ecr.region.amazonaws.com/my-lambda-container:tag"
+# Example usage: ./delpoy.sh "my-lambda-function" "account-id.dkr.ecr.region.amazonaws.com/my-lambda-container:tag"
 
 FUNCTION_NAME=$1
 IMAGE_URI=$2
 
 response=$(aws lambda update-function-code --function-name $FUNCTION_NAME --image-uri $IMAGE_URI)
-status=$(echo $response | jq '.LastUpdateStatus')
 
-echo "Lambda deployment status: $status"
+aws lambda wait function-updated-v2 --function-name $FUNCTION_NAME
+
+echo "Container image has been deployed to Lambda."
+
+exit(0)
