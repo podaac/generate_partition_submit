76249e8100d8021c446c54f3209fdde86c71a741
Nikki Tebaldi <17799906+nikki-t@users.noreply.github.com>
2023-04-25 17:58:41 +0000

    Create ECR repo if it does not exist

--- deploy-lambda.sh
+++ deploy-lambda.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #
-# Script to deply a container image to an AWS Lambda Function
+# Script to deploy a container image to an AWS Lambda Function
 #
 # Command line arguments:
 # [1] function_name: Name of AWS Lambda function name
 # [2] image_uri: URI of container of image
 # 
-# Example usage: ./delpoy.sh "my-lambda-function" "account-id.dkr.ecr.region.amazonaws.com/my-lambda-container:tag"
+# Example usage: ./delpoy-lambda.sh "my-lambda-function" "account-id.dkr.ecr.region.amazonaws.com/my-lambda-container:tag"
 
 FUNCTION_NAME=$1
 IMAGE_URI=$2
