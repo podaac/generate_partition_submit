298b0376a62c8f10877d2c9c6a33578adf54eba0
simonhkhan <shkhanservices@gmail.com>
2025-07-13 23:54:00 -0700

    Updated config.json for fargate resources
    - Added source hash to terraform file so it detects changes from the json file.

--- terraform/partition_submit-lambda.tf
+++ terraform/partition_submit-lambda.tf
@@ -25,6 +25,7 @@ resource "aws_s3_object" "aws_s3_bucket_job_configuration" {
   key                    = "config/job_config.json"
   server_side_encryption = "aws:kms"
   source                 = "job_config.json"
+  source_hash            = filemd5("job_config.json")
 }
 
 # Lambda resource-based policy
